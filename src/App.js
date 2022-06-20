0510d275497f41b38ccbb91e300af187e82d73f6
linyc <barney0817.cs10@nycu.edu.tw>
2022-06-21 00:21:54 +0800

    Test second time

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Build Pending ~
+          Build Pending ~~
         </a>
       </header>
     </div>
